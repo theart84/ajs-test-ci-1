6341a44f24316b60a146be720c073d99d2cc0dd1
theart84 <artshadow74@gmail.com>
2021-02-11 17:27:44 +0500

    Task completed

---
+++ src/js/player.js
@@ -0,0 +1,9 @@
+export default function checkHealth(player) {
+  if (player.health > 50) {
+    return 'healthy';
+  }
+  if (player.health >= 15 && player.health <= 50) {
+    return 'wounded';
+  }
+  return 'critical';
+}
---
+++ src/test/player.test.js
@@ -0,0 +1,17 @@
+import checkHealth from "../js/player";
+
+test('Передаем значение больше 50', () => {
+  expect(checkHealth({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+test('Передаем значение 30', () => {
+  expect(checkHealth({ name: 'Маг', health: 30 })).toBe('wounded');
+});
+test('Передаем значение меньше 15', () => {
+  expect(checkHealth({ name: 'Маг', health: 10 })).toBe('critical');
+});
+test('Передаем значение равное 15', () => {
+  expect(checkHealth({ name: 'Маг', health: 15 })).toBe('wounded');
+});
+test('Передаем значение равное 50', () => {
+  expect(checkHealth({ name: 'Маг', health: 50 })).toBe('wounded');
+});
